96e74d8e9ac7c1e62dc1c5ff1a80a25ffb52ba13
YukiKuramoto <otomaru.kcufs@i.softbank.jp>
2021-12-12 20:33:40 +0900

    ユーザー一覧ページにて自アカウントを表示しないよう機能修正

--- resources/js/components/itemcomponents/useritem.vue
+++ resources/js/components/itemcomponents/useritem.vue
@@ -14,7 +14,7 @@
       </div>
     </v-app>
     <div class="users-contents-wrapper">
-      <div v-for="user in page_users" class="users-item-wrapper">
+      <div v-for="user in page_users" v-if="user.id != login_user" class="users-item-wrapper">
         <a :href="'/mypage/' + user.id"  class="anker-area">
           <div class="user-image-area">
             <img v-if="user.image_path !== null" :src="user.image_path">
@@ -63,7 +63,7 @@ export default {
         }
     },
     created: function(){
-      console.log(this.response);
+      console.log(this);
       this.page_current = 1;
       this.page_length = this.length;
       this.page_users = this.response;
